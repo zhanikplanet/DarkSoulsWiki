d0fc0bc695da123501a36068139ca61f4bcaa818
zhanikplanet <zhanik.planet1@gmail.com>
2025-01-28 13:07:59 +0500

    using routes for redirect

---
+++ dark-souls-wiki/src/app/dark-souls-1/dark-souls-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-souls-1',
+  imports: [],
+  templateUrl: './dark-souls-1.component.html',
+  styleUrl: './dark-souls-1.component.scss'
+})
+export class DarkSouls1Component {
+
+}
